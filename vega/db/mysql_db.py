e6b6a628386dcc0e5d3b165e7644d8f74da70046
SuperAllenYao <gdjmywl@gmail.com>
2019-07-20 11:48:02 +0700

    新闻管理后台系统(第一阶段)

---
+++ vega/db/mysql_db.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import mysql.connector.pooling
+
+__config = {
+    "host": "localhost",
+    "port": 3306,
+    "user": "root",
+    "password": "478890762",
+    "database": "vega"
+}
+try:
+    pool = mysql.connector.pooling.MySQLConnectionPool(
+        **__config,
+        pool_size=10
+    )
+except Exception as e:
+    print(e)
